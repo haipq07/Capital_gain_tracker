383e4a68c8cbf2aa7de15b1ca97ec3521a90613b
haipq07 <haipq07@gmail.com>
2025-02-17 15:46:32 -0800

    Create stock_purchase.py

---
+++ stock_purchase.py
@@ -0,0 +1,7 @@
+# stock_purchase.py
+
+class StockPurchase:
+    def __init__(self, stock_symbol, cost_per_share, shares):
+        self.stock_symbol = stock_symbol
+        self.cost_per_share = cost_per_share
+        self.shares = shares
